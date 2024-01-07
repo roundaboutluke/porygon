c1d899cfa60a90a4f230edc4058ccaf0a831cbe9
arniethepie <arniethepie@gmail.com>
2024-01-07 21:16:16 +0000

    abstracted api query

--- Porygon.go
+++ Porygon.go
@@ -1,53 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/dustin/go-humanize"
 	_ "github.com/go-sql-driver/mysql"
-	"io"
-	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"Porygon/config"
+	"Porygon/query"
 )
-
-type ApiResponse struct {
-	SpawnId int `json:"spawn_id"`
-}
-
-type Query struct {
-	Min struct {
-		Latitude  float64 `json:"latitude"`
-		Longitude float64 `json:"longitude"`
-	} `json:"min"`
-	Max struct {
-		Latitude  float64 `json:"latitude"`
-		Longitude float64 `json:"longitude"`
-	} `json:"max"`
-	Filters []struct {
-		Pokemon []struct {
-			Id int `json:"id"`
-		} `json:"pokemon"`
-		AtkIv struct {
-			Min int `json:"min"`
-			Max int `json:"max"`
-		} `json:"atk_iv"`
-		DefIv struct {
-			Min int `json:"min"`
-			Max int `json:"max"`
-		} `json:"def_iv"`
-		StaIv struct {
-			Min int `json:"min"`
-			Max int `json:"max"`
-		} `json:"sta_iv"`
-	} `json:"filters"`
-}
 
 type Incident struct {
 	ID    int
@@ -76,110 +42,6 @@ func formatEmoji(emoji string) string {
 		return "<" + emoji + ">"
 	}
 	return emoji
-}
-
-func apiRequest(config config.Config, ivMin, ivMax int) ([]ApiResponse, error) {
-	query := Query{
-		Min: struct {
-			Latitude  float64 `json:"latitude"`
-			Longitude float64 `json:"longitude"`
-		}{
-			Latitude:  config.Coordinates.Min.Latitude,
-			Longitude: config.Coordinates.Min.Longitude,
-		},
-		Max: struct {
-			Latitude  float64 `json:"latitude"`
-			Longitude float64 `json:"longitude"`
-		}{
-			Latitude:  config.Coordinates.Max.Latitude,
-			Longitude: config.Coordinates.Max.Longitude,
-		},
-		Filters: []struct {
-			Pokemon []struct {
-				Id int `json:"id"`
-			} `json:"pokemon"`
-			AtkIv struct {
-				Min int `json:"min"`
-				Max int `json:"max"`
-			} `json:"atk_iv"`
-			DefIv struct {
-				Min int `json:"min"`
-				Max int `json:"max"`
-			} `json:"def_iv"`
-			StaIv struct {
-				Min int `json:"min"`
-				Max int `json:"max"`
-			} `json:"sta_iv"`
-		}{
-			{
-				Pokemon: func() []struct {
-					Id int `json:"id"`
-				} {
-					pokemon := make([]struct {
-						Id int `json:"id"`
-					}, 1015)
-					for i := range pokemon {
-						pokemon[i].Id = i + 1
-					}
-					return pokemon
-				}(),
-				AtkIv: struct {
-					Min int `json:"min"`
-					Max int `json:"max"`
-				}{
-					Min: ivMin,
-					Max: ivMax,
-				},
-				DefIv: struct {
-					Min int `json:"min"`
-					Max int `json:"max"`
-				}{
-					Min: ivMin,
-					Max: ivMax,
-				},
-				StaIv: struct {
-					Min int `json:"min"`
-					Max int `json:"max"`
-				}{
-					Min: ivMin,
-					Max: ivMax,
-				},
-			},
-		},
-	}
-
-	jsonData, err := json.Marshal(query)
-	if err != nil {
-		return nil, fmt.Errorf("error converting query to JSON: %w", err)
-	}
-
-	req, err := http.NewRequest("POST", config.API.URL+"/api/pokemon/v2/scan", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return nil, fmt.Errorf("error creating API request: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	if config.API.Secret != "" {
-		req.Header.Set("X-Golbat-Secret", config.API.Secret)
-	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error making API request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading API response: %w", err)
-	}
-
-	var apiResponses []ApiResponse
-	err = json.Unmarshal(body, &apiResponses)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing API response: %w", err)
-	}
-
-	return apiResponses, nil
 }
 
 func saveMessageIDs(filename string, messageIDs map[string]string) {
@@ -221,6 +83,7 @@ func loadMessageIDs(filename string) map[string]string {
 
 func main() {
 	var config config.Config
+
 	if err := config.ParseConfig(); err != nil {
 		panic(err)
 	}
@@ -412,7 +275,7 @@ func main() {
 
 			var hundoActiveCount, nundoActiveCount int
 			if config.Config.IncludeActiveCounts {
-				hundoApiResponses, err := apiRequest(config, 15, 15)
+				hundoApiResponses, err := query.ApiRequest(config, 15, 15)
 				if err != nil {
 					fmt.Println(err)
 					db.Close()
@@ -425,7 +288,7 @@ func main() {
 				}
 				hundoActiveCount = len(hundoSpawnIds)
 
-				nundoApiResponses, err := apiRequest(config, 0, 0)
+				nundoApiResponses, err := query.ApiRequest(config, 0, 0)
 				if err != nil {
 					fmt.Println(err)
 					db.Close()
---
+++ query/query.go
@@ -0,0 +1,147 @@
+package query
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"Porygon/config"
+)
+
+type apiResponse struct {
+	SpawnId int `json:"spawn_id"`
+}
+
+type query struct {
+	Min struct {
+		Latitude  float64 `json:"latitude"`
+		Longitude float64 `json:"longitude"`
+	} `json:"min"`
+	Max struct {
+		Latitude  float64 `json:"latitude"`
+		Longitude float64 `json:"longitude"`
+	} `json:"max"`
+	Filters []struct {
+		Pokemon []struct {
+			Id int `json:"id"`
+		} `json:"pokemon"`
+		AtkIv struct {
+			Min int `json:"min"`
+			Max int `json:"max"`
+		} `json:"atk_iv"`
+		DefIv struct {
+			Min int `json:"min"`
+			Max int `json:"max"`
+		} `json:"def_iv"`
+		StaIv struct {
+			Min int `json:"min"`
+			Max int `json:"max"`
+		} `json:"sta_iv"`
+	} `json:"filters"`
+}
+
+func ApiRequest(config config.Config, ivMin, ivMax int) ([]apiResponse, error) {
+	query := query{
+		Min: struct {
+			Latitude  float64 `json:"latitude"`
+			Longitude float64 `json:"longitude"`
+		}{
+			Latitude:  config.Coordinates.Min.Latitude,
+			Longitude: config.Coordinates.Min.Longitude,
+		},
+		Max: struct {
+			Latitude  float64 `json:"latitude"`
+			Longitude float64 `json:"longitude"`
+		}{
+			Latitude:  config.Coordinates.Max.Latitude,
+			Longitude: config.Coordinates.Max.Longitude,
+		},
+		Filters: []struct {
+			Pokemon []struct {
+				Id int `json:"id"`
+			} `json:"pokemon"`
+			AtkIv struct {
+				Min int `json:"min"`
+				Max int `json:"max"`
+			} `json:"atk_iv"`
+			DefIv struct {
+				Min int `json:"min"`
+				Max int `json:"max"`
+			} `json:"def_iv"`
+			StaIv struct {
+				Min int `json:"min"`
+				Max int `json:"max"`
+			} `json:"sta_iv"`
+		}{
+			{
+				Pokemon: func() []struct {
+					Id int `json:"id"`
+				} {
+					pokemon := make([]struct {
+						Id int `json:"id"`
+					}, 1015)
+					for i := range pokemon {
+						pokemon[i].Id = i + 1
+					}
+					return pokemon
+				}(),
+				AtkIv: struct {
+					Min int `json:"min"`
+					Max int `json:"max"`
+				}{
+					Min: ivMin,
+					Max: ivMax,
+				},
+				DefIv: struct {
+					Min int `json:"min"`
+					Max int `json:"max"`
+				}{
+					Min: ivMin,
+					Max: ivMax,
+				},
+				StaIv: struct {
+					Min int `json:"min"`
+					Max int `json:"max"`
+				}{
+					Min: ivMin,
+					Max: ivMax,
+				},
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(query)
+	if err != nil {
+		return nil, fmt.Errorf("error converting query to JSON: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", config.API.URL+"/api/pokemon/v2/scan", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("error creating API request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if config.API.Secret != "" {
+		req.Header.Set("X-Golbat-Secret", config.API.Secret)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making API request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading API response: %w", err)
+	}
+
+	var apiResponses []apiResponse
+	err = json.Unmarshal(body, &apiResponses)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing API response: %w", err)
+	}
+
+	return apiResponses, nil
+}
